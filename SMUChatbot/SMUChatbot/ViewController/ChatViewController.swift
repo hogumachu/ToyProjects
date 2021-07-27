33bc6665a1c82f803aa4c97acf3f17c675982f40
hogumachu <castle_jun@daum.net>
2021-07-27 18:16:51 +0900

    Todo: Keyboard Will Show & Hide -> Change Text View Height

--- SMUChatbot/SMUChatbot/ViewController/ChatViewController.swift
+++ SMUChatbot/SMUChatbot/ViewController/ChatViewController.swift
@@ -67,7 +67,6 @@ class ChatViewController: UIViewController {
         view.backgroundColor = .white
         
         setConstraints()
-        
         let result = sendButton.rx.tap.asDriver()
             .flatMapLatest {
                 self.model.responseDjango(sendText: self.chatTextField.text ?? "")
@@ -77,30 +76,76 @@ class ChatViewController: UIViewController {
         result
             .drive(textView.rx.text)
             .disposed(by: disposeBag)
+        
+//        keyboardHeight()
+//            .subscribe(onNext: { [weak self] height in
+//                UIView.animate(withDuration: 0.3) {
+//                    self?.textView.contentInset.bottom = height
+//                }
+//
+//            })
+//            .disposed(by: disposeBag)
+        
+        let keyboardWillShowNotiObservable = NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+            .map { ($0.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height ?? 0}
+        let keyboardWillHideNotiObservable = NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
+            .map { notification -> CGFloat in 0}
+
+        Observable.merge(keyboardWillShowNotiObservable, keyboardWillHideNotiObservable)
+            .map { [unowned self] height -> UIEdgeInsets in
+                var inset = self.textView.contentInset
+                inset.bottom = height
+                return inset
+            }
+            .subscribe(onNext: { [weak self] inset in
+                print(inset)
+                print(inset.bottom)
+                self?.textView.contentInset = inset
+                UIView.animate(withDuration: 0.3) {
+                    self?.textView.layoutIfNeeded()
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        
     }
     
     func setConstraints() {
-        view.initAutoLayout(UIViews: [mainViewButton, chatTextField, sendButton, textView])
+        textView.initAutoLayout(UIViews: [chatTextField, sendButton])
+        view.initAutoLayout(UIViews: [mainViewButton, textView])
         NSLayoutConstraint.activate([
             mainViewButton.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
             mainViewButton.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 10),
             
-            chatTextField.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
-            chatTextField.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            chatTextField.leadingAnchor.constraint(equalTo: textView.layoutMarginsGuide.leadingAnchor),
+            chatTextField.bottomAnchor.constraint(equalTo: textView.layoutMarginsGuide.bottomAnchor),
             chatTextField.trailingAnchor.constraint(equalTo: sendButton.leadingAnchor, constant: -5),
             chatTextField.heightAnchor.constraint(equalTo: sendButton.heightAnchor),
             
-            sendButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            sendButton.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            sendButton.trailingAnchor.constraint(equalTo: textView.layoutMarginsGuide.trailingAnchor),
+            sendButton.bottomAnchor.constraint(equalTo: textView.layoutMarginsGuide.bottomAnchor),
             sendButton.widthAnchor.constraint(equalToConstant: 60),
             sendButton.heightAnchor.constraint(equalToConstant: 40),
             
             textView.topAnchor.constraint(equalTo: mainViewButton.bottomAnchor, constant: 10),
             textView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
             textView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
-            textView.bottomAnchor.constraint(equalTo: sendButton.topAnchor, constant: -10)
+            textView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor)
         ])
     }
     
-
+    func keyboardHeight() -> Observable<CGFloat> {
+        return Observable
+                .from([
+                    NotificationCenter.default.rx.notification(UIResponder.keyboardWillShowNotification)
+                                .map { notification -> CGFloat in
+                                    (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height ?? 0
+                                },
+                        NotificationCenter.default.rx.notification(UIResponder.keyboardWillHideNotification)
+                                .map { _ -> CGFloat in
+                                    0
+                                }
+                ])
+                .merge()
+    }
 }
