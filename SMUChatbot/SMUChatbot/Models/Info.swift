b7fe972e5c111a9c33f3920243f9eda81e2b5adf
hogumachu <castle_jun@daum.net>
2021-08-05 03:00:29 +0900

    Change Info Model Sample

--- SMUChatbot/SMUChatbot/Models/Info.swift
+++ SMUChatbot/SMUChatbot/Models/Info.swift
@@ -1,18 +1,17 @@
 import Foundation
 
 struct Info {
-    let summary: String
+    let title: String
     let detailInfo: String
-    
 }
 
 let myChatbotInfo = [
-    Info(summary: "1", detailInfo: "detailInfo 1"),
-    Info(summary: "2", detailInfo: "detailInfo 2"),
-    Info(summary: "3", detailInfo: "detailInfo 3"),
-    Info(summary: "4", detailInfo: "detailInfo 4"),
-    Info(summary: "5", detailInfo: "detailInfo 5"),
-    Info(summary: "6", detailInfo: "detailInfo 6"),
-    Info(summary: "7", detailInfo: "detailInfo 7"),
-    Info(summary: "챗봇 시작하기", detailInfo: "챗봇을 시작합니다"),
+    Info(title: "1", detailInfo: "detailInfo 1"),
+    Info(title: "2", detailInfo: "detailInfo 2"),
+    Info(title: "3", detailInfo: "detailInfo 3"),
+    Info(title: "4", detailInfo: "detailInfo 4"),
+    Info(title: "5", detailInfo: "detailInfo 5"),
+    Info(title: "6", detailInfo: "detailInfo 6"),
+    Info(title: "7", detailInfo: "detailInfo 7"),
+    Info(title: "시작", detailInfo: "챗봇을 시작합니다"),
 ]
