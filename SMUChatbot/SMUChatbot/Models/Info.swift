97b0efcaedb3a519fe2411c042f46308609e775a
hogumachu <castle_jun@daum.net>
2021-08-11 23:22:03 +0900

    Change Test Data

--- SMUChatbot/SMUChatbot/Models/Info.swift
+++ SMUChatbot/SMUChatbot/Models/Info.swift
@@ -7,12 +7,12 @@ struct Info {
 }
 
 let myChatbotInfo = [
-    Info(title: "1", detailInfo: "detailInfo 1", color: .purple),
-    Info(title: "2", detailInfo: "detailInfo 2", color: .systemPink),
-    Info(title: "3", detailInfo: "detailInfo 3", color: .systemRed),
-    Info(title: "4", detailInfo: "detailInfo 4", color: .brown),
-    Info(title: "5", detailInfo: "detailInfo 5", color: .cyan),
-    Info(title: "6", detailInfo: "detailInfo 6", color: .yellow),
-    Info(title: "7", detailInfo: "detailInfo 7", color: .darkGray),
+//    Info(title: "1", detailInfo: "detailInfo 1", color: .purple),
+//    Info(title: "2", detailInfo: "detailInfo 2", color: .systemPink),
+//    Info(title: "3", detailInfo: "detailInfo 3", color: .systemRed),
+//    Info(title: "4", detailInfo: "detailInfo 4", color: .brown),
+//    Info(title: "5", detailInfo: "detailInfo 5", color: .cyan),
+//    Info(title: "6", detailInfo: "detailInfo 6", color: .yellow),
+//    Info(title: "7", detailInfo: "detailInfo 7", color: .darkGray),
     Info(title: "시작", detailInfo: "챗봇을 시작합니다", color: .magenta),
 ]
