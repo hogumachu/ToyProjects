e27971db6eeb44ea355b8e4d28a79c7e46f76201
hogumachu <castle_jun@daum.net>
2021-08-12 18:13:33 +0900

    Add ChatBubbleView

--- SMUChatbot/SMUChatbot/View/ChatTableViewCell.swift
+++ SMUChatbot/SMUChatbot/View/ChatTableViewCell.swift
@@ -3,6 +3,12 @@ import UIKit
 class ChatTableViewCell: UITableViewCell {
     static let identifier = "ChatTableViewCell"
     let chatLabel = DetailLabel()
+    let chatBubbleView: UIView = {
+        let uiView = UIView()
+        uiView.layer.masksToBounds = true
+        uiView.layer.cornerRadius = 8
+        return uiView
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -13,32 +19,38 @@ class ChatTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    
     func configureUI() {
         chatLabel.font = .systemFont(ofSize: 15)
         chatLabel.textColor = .black
         chatLabel.numberOfLines = 0
         chatLabel.lineBreakMode = .byWordWrapping
-        chatLabel.layer.masksToBounds = true
-        chatLabel.layer.cornerRadius = 5
+        chatLabel.backgroundColor = .clear
         
         backgroundColor = .clear
-        contentView.initAutoLayout(UIViews: [chatLabel])
+        contentView.initAutoLayout(UIViews: [chatBubbleView, chatLabel])
         
-        chatLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5).isActive = true
+        
+        chatBubbleView.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+        chatBubbleView.bottomAnchor.constraint(equalTo: chatLabel.bottomAnchor, constant: 5).isActive = true
+        
+        chatLabel.topAnchor.constraint(equalTo: chatBubbleView.topAnchor, constant: 5).isActive = true
+        chatLabel.leadingAnchor.constraint(equalTo: chatBubbleView.leadingAnchor, constant: 8).isActive = true
+        chatLabel.trailingAnchor.constraint(equalTo: chatBubbleView.trailingAnchor, constant: -8).isActive = true
+        
         contentView.heightAnchor.constraint(equalTo: chatLabel.heightAnchor, constant: 10).isActive = true
     }
     
     func isSender(_ compare: Bool) {
         if compare {
-            chatLabel.textAlignment = .right
-            chatLabel.backgroundColor = .yellow
-            chatLabel.leadingAnchor.constraint(greaterThanOrEqualTo: leadingAnchor, constant: 10).isActive = true
-            chatLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
+            chatBubbleView.backgroundColor = .yellow
+            chatBubbleView.leadingAnchor.constraint(greaterThanOrEqualTo: leadingAnchor, constant: 10).isActive = true
+            chatBubbleView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
         } else {
-            chatLabel.textAlignment = .left
-            chatLabel.backgroundColor = .brown
-            chatLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
-            chatLabel.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor, constant: -10).isActive = true
+            chatBubbleView.backgroundColor = .white
+            chatBubbleView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
+            chatBubbleView.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor, constant: -10).isActive = true
         }
     }
     
