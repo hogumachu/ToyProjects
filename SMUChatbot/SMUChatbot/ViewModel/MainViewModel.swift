a30b546142054b8840dbe51a1835ba2863c66865
hogumachu <castle_jun@daum.net>
2021-08-16 12:32:14 +0900

    Add Comment + Remove Alpha, textColor Set

--- SMUChatbot/SMUChatbot/ViewModel/MainViewModel.swift
+++ SMUChatbot/SMUChatbot/ViewModel/MainViewModel.swift
@@ -1,11 +1,10 @@
 import RxSwift
 
 class MainViewModel {
+    // Animation 진행 전에 alpha 를 0 으로 설정해야 정상적으로 동작함
     func appear(_ label: UILabel, duration: TimeInterval) -> Observable<Void> {
         return Observable.create { observer -> Disposable in
-            label.alpha = 0
             UIView.animate(withDuration: duration, animations: {
-                label.textColor = .white
                 label.alpha = 1
             }, completion: { _ in
                 observer.onNext(())
