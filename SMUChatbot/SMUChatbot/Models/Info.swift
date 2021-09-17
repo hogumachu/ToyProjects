9ace81c92465ab732b7d031173ec137c2b97ccbd
hogumachu <castle_jun@daum.net>
2021-09-17 17:30:35 +0900

    Change Info struct

--- SMUChatbot/SMUChatbot/Models/Info.swift
+++ SMUChatbot/SMUChatbot/Models/Info.swift
@@ -4,16 +4,10 @@ struct Info {
     let title: String
     let detailInfo: String
     let color: UIColor
-    let detailView: UIView
 }
 
 let myChatbotInfo = [
-//    Info(title: "Chatbot", detailInfo: "챗봇이란", color: .systemPink, detailView: UIView()),
-    Info(title: "Team ChattingHaeJo", detailInfo: "채팅해조 팀이란", color: .darkGray, detailView: teamChattingHaeJoView()),
-    Info(title: "How to use", detailInfo: "채팅해조 챗봇 사용법", color: .systemIndigo, detailView: UIView()),
-    Info(title: "Start", detailInfo: "챗봇 시작하기", color: .brown, detailView: UIView()),
+    Info(title: "Team ChattingHaeJo", detailInfo: "채팅해조 팀이란", color: .darkGray),
+    Info(title: "How to use", detailInfo: "채팅해조 챗봇 사용법", color: .systemIndigo),
+    Info(title: "Start", detailInfo: "챗봇 시작하기", color: .brown),
 ]
-
-
-
-
