2f738538ccb6a0bd92e23009d09aeb7386695431
hogumachu <castle_jun@daum.net>
2021-09-23 20:32:17 +0900

    Add Space

--- SMUChatbot/SMUChatbot/Models/Info.swift
+++ SMUChatbot/SMUChatbot/Models/Info.swift
@@ -7,7 +7,7 @@ struct Info {
 }
 
 let myChatbotInfo = [
-    Info(title: "Team ChattingHaeJo", detailInfo: "채팅해조 팀이란", color: .darkGray),
+    Info(title: "Team Chatting HaeJo", detailInfo: "채팅해조 팀이란", color: .darkGray),
     Info(title: "How to use", detailInfo: "채팅해조 챗봇 사용법", color: .systemIndigo),
     Info(title: "Start", detailInfo: "챗봇 시작하기", color: .brown),
 ]
