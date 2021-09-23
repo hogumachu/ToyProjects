16b404f54b4e63bf3efdaae2d82f1caf31123804
hogumachu <castle_jun@daum.net>
2021-09-23 20:29:44 +0900

    Create InfoDetailTeamViewModel

---
+++ SMUChatbot/SMUChatbot/ViewModel/InfoDetailTeamViewModel.swift
@@ -0,0 +1,26 @@
+import Foundation
+import Kingfisher
+class InfoDetailTeamViewModel {
+    enum Page {
+        case chatViewController
+        case inPage
+        case popViewController
+    }
+    let info = detailTeamInfo
+    
+    func downloadImage(urlString: String) -> ImageResource? {
+        guard let url = URL(string: urlString) else { return nil }
+        let resource = ImageResource(downloadURL: url)
+        return resource
+    }
+    
+    func changePage(next: Int) -> Page {
+        if next < info.count && next >= 0 {
+            return .inPage
+        } else if next == -1 {
+            return .popViewController
+        } else {
+            return .chatViewController
+        }
+    }
+}
