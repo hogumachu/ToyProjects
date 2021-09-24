877d25d385fc60ee956e230833e86cb400514050
hogumachu <castle_jun@daum.net>
2021-09-24 17:05:51 +0900

    Add detailTeamInfo, DetailUseInfo

--- SMUChatbot/SMUChatbot/Models/Description.swift
+++ SMUChatbot/SMUChatbot/Models/Description.swift
@@ -3,8 +3,15 @@ struct Description {
     let description: String
 }
 
-let detailTeamInfo = [
+let detailTeamInfo = ["https://user-images.githubusercontent.com/74225754/134623586-5bd7415d-9f1a-419c-a684-de133ec4ea1e.gif",
+                      "https://user-images.githubusercontent.com/74225754/134623584-8d2d5cd6-0918-4d76-b01e-96d02380e4d8.gif",
+                      "https://user-images.githubusercontent.com/74225754/134623581-a49838a8-bf55-4f9c-bd3e-58ae1c9b39be.gif",
+                      "https://user-images.githubusercontent.com/74225754/134633678-4cc43e7f-cdec-43e9-8d8c-6caca96cec69.gif"]
+
+let detailUseInfo = [
     Description(imageUrlString: "https://user-images.githubusercontent.com/74225754/133364875-b18ca2e5-85da-479d-856c-e008440e82bf.gif", description: "➀ 이전 페이지에서 우측으로 이동합니다"),
     Description(imageUrlString: "https://user-images.githubusercontent.com/74225754/133364871-6711d6dd-aa79-4996-b74a-6b1cc5dc4196.gif", description: "➁ 챗봇에게 학사 정보를 질문합니다"),
     Description(imageUrlString: "https://user-images.githubusercontent.com/74225754/134494250-15ee1138-0bbf-4c9f-8a51-c6bd88d819b0.gif", description: "➂ 다음 버튼을 눌러 챗봇을 시작합니다")
 ]
+
+
