589ea888fc1d83930be67a5c9a9940e5a0337db1
hogumachu <castle_jun@daum.net>
2021-09-24 17:06:11 +0900

    Update ChatViewController.swift

--- SMUChatbot/SMUChatbot/ViewController/ChatViewController.swift
+++ SMUChatbot/SMUChatbot/ViewController/ChatViewController.swift
@@ -94,13 +94,11 @@ class ChatViewController: BaseViewController {
            
             if item.isSender {
                 let cell = tableViewCell.dequeueReusableCell(withIdentifier: ChatTableViewSenderCell.identifier, for: IndexPath.init(row: row, section: 0)) as! ChatTableViewSenderCell
-                
                 cell.chatLabel.text = item.text
                 cell.dateLabel.text = item.dateString
                 return cell
             } else {
                 let cell = tableViewCell.dequeueReusableCell(withIdentifier: ChatTableViewReceiverCell.identifier, for: IndexPath.init(row: row, section: 0)) as! ChatTableViewReceiverCell
-                
                 cell.chatLabel.text = item.text
                 cell.dateLabel.text = item.dateString
                 return cell
