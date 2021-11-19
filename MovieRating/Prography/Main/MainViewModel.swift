98174d6e04fc946749bb05e7b37bd69305231a49
hogumachu <castle_jun@daum.net>
2021-11-19 17:41:29 +0900

    Add loading

--- MovieRating/Prography/Main/MainViewModel.swift
+++ MovieRating/Prography/Main/MainViewModel.swift
@@ -4,8 +4,13 @@ class MainViewModel: ViewModelType {
     var coordinator: Coordinator?
     var startLoading = {}
     var endLoading = {}
+    private var loading = false
     
     func next(score: String?) {
+        if loading {
+            return
+        }
+        loading = true
         startLoading()
         
         Repository.shared.execute(url: Repository.shared.generateURL(score: score ?? "0", page: 1)) { [weak self] (result: Result<MovieResponse, Error>) in
@@ -14,12 +19,15 @@ class MainViewModel: ViewModelType {
                 if let movies = success.data?.movies {
                     DispatchQueue.main.async {
                         self?.endLoading()
+                        self?.loading = false
                         self?.coordinator?.movieList(movies: movies, page: success.data?.page_number ?? 1, score: score ?? "0")
                     }
                 } else {
+                    self?.loading = false
                     self?.endLoading()
                 }
             case .failure(let failure):
+                self?.loading = false
                 self?.endLoading()
                 print(failure.localizedDescription)
             }
