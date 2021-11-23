8b06be96bddc1d086798da0dd641a8b7be9caff2
hogumachu <castle_jun@daum.net>
2021-11-23 13:56:25 +0900

    Image Load Async

--- MovieRating/Prography/Repository/ImageLoader.swift
+++ MovieRating/Prography/Repository/ImageLoader.swift
@@ -1,50 +1,68 @@
 import UIKit.UIImage
 
 class ImageLoader {
-    
     static func fetchImage(url: String, completion: @escaping (UIImage?) -> Void) {
-        if url.isEmpty {
-            completion(nil)
-            return
+        DispatchQueue.global().async {
+            if url.isEmpty {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                
+                return
+            }
+            
+            if let image = ImageCacheManager.shared.loadImage(url) {
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+                return
+            }
+            
+            URLSession.shared.dataTask(with: URL(string: url)!) { data, response, error in
+                if let _ = error {
+                    DispatchQueue.main.async {
+                        completion(nil)
+                    }
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse else {
+                    DispatchQueue.main.async {
+                        completion(nil)
+                    }
+                    return
+                }
+                
+                guard (200..<300).contains(response.statusCode) else {
+                    DispatchQueue.main.async {
+                        completion(nil)
+                    }
+                    return
+                }
+                
+                guard let data = data else {
+                    DispatchQueue.main.async {
+                        completion(nil)
+                    }
+                    return
+                }
+                
+                let image = UIImage(data: data)
+                
+                guard let image = image else {
+                    DispatchQueue.main.async {
+                        completion(nil)
+                    }
+                    return
+                }
+                
+                ImageCacheManager.shared.saveImage(url, image: image)
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+            }
+            .resume()
         }
-        
-        if let image = ImageCacheManager.shared.loadImage(url) {
-            completion(image)
-            return
-        }
-        
-        URLSession.shared.dataTask(with: URL(string: url)!) { data, response, error in
-            if let _ = error {
-                completion(nil)
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse else {
-                completion(nil)
-                return
-            }
-            
-            guard (200..<300).contains(response.statusCode) else {
-                completion(nil)
-                return
-            }
-            
-            guard let data = data else {
-                completion(nil)
-                return
-            }
-            
-            let image = UIImage(data: data)
-            
-            guard let image = image else {
-                completion(nil)
-                return
-            }
-            
-            ImageCacheManager.shared.saveImage(url, image: image)
-            completion(image)
-        }
-        .resume()
         
     }
 }
