bbaef53ddcb20e9cf9d5b7930ac6862a263d0034
hogumachu <castle_jun@daum.net>
2021-11-23 13:03:23 +0900

    Change Dependency

--- MovieRating/Prography/MovieList/MovieListViewModel.swift
+++ MovieRating/Prography/MovieList/MovieListViewModel.swift
@@ -1,71 +1,50 @@
 import Foundation
-import UIKit
 
-class MovieListViewModel: ViewModelType {
+class MovieListViewModel: StorableViewModelType {
     struct Dependency {
-        let movies: [Movie]
-        let page: Int
+        let coordinator: Coordinator
+        let storage: MovieStorageType
         let score: String
     }
-    var coordinator: Coordinator?
-    private var movies: [Movie]
-    private var page: Int
-    private let score: String
     
-    var startLoading = {}
-    var endLoading = {}
-    private var loading = false
+    let coordinator: Coordinator
+    let storage: MovieStorageType
+    let score: String
+    var loading = false
     
     init(dependency: Dependency) {
-        self.movies = dependency.movies
-        self.page = dependency.page
+        self.coordinator = dependency.coordinator
+        self.storage = dependency.storage
         self.score = dependency.score
     }
     
-    func movie(at index: Int) -> Movie {
-        return movies[index]
-    }
-    
-    func movieCount() -> Int {
-        return movies.count
-    }
-    
-    func detail(movie: Movie) {
-        coordinator?.movieDetail(movie: movie)
+    func detail(at index: Int) {
+        let movie = storage.movie(at: index)
+        coordinator.movieDetail(movie: movie)
     }
     
     func next() {
         if loading {
             return
         }
-        print(page)
-        loading = true
-        page += 1
-        startLoading()
         
-        Repository.shared.execute(url: Repository.shared.generateURL(score: score, page: page)) { [weak self] (result: Result<MovieResponse, Error>) in
+        loading = true
+        
+        Repository.shared.execute(url: Repository.shared.generateURL(score: score, page: storage.currentPage())) { [weak self] (result: Result<MovieResponse, Error>) in
             switch result {
             case .success(let success):
                 if let movies = success.data?.movies {
                     DispatchQueue.main.async {
-                        print("Success")
-                        self?.loading = false
-                        self?.movies.append(contentsOf: movies)
-                        self?.endLoading()
+                        self?.storage.addMovies(movies: movies)
                     }
                 } else {
-                    DispatchQueue.main.async {
-                        self?.loading = false
-                        self?.endLoading()
-                    }
+                    print("Next Page, Empty Movies")
                 }
             case .failure(let failure):
-                DispatchQueue.main.async {
-                    self?.loading = false
-                    self?.endLoading()
-                }
                 print(failure.localizedDescription)
             }
+            
+            self?.loading = false
         }
     }
 }
