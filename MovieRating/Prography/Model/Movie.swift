8850f6b507a9c7e966da6cdf4a70eedfe375b0ba
hogumachu <castle_jun@daum.net>
2021-11-23 13:04:25 +0900

    Create Empty Property

--- MovieRating/Prography/Model/Movie.swift
+++ MovieRating/Prography/Model/Movie.swift
@@ -6,3 +6,7 @@ struct Movie: Codable {
     let large_cover_image: String?
     let background_image: String?
 }
+
+extension Movie {
+    static let Empty = Movie.init(title: "", rating: 0, large_cover_image: "", background_image: "")
+}
